6b308a69ea04ab7521a6edb8c3ae8183878ee2c8
Felipe Caleffi <felipe.caleffi@ufsm.br>
2026-03-28 15:58:23 -0300

    Adiciona Simulação 4

--- js/data.js
+++ js/data.js
@@ -1,4 +1,45 @@
 const SIMULATION_DATA = {
+  sim4: {
+    label: "Simulação n\u00ba 4",
+    direction: "Sentido Oeste",
+    info: {
+      rodovia: "BR-277",
+      kmInicial: "Km 76",
+      kmFinal: "Km 78",
+      pista: "Oeste",
+      faixaInterrompida: "Faixa 1",
+      horario: "9h às 18h"
+    },
+    atraso: {
+      rows: [
+        { hora: "9h",  display: "00:03:10", seconds: 190 },
+        { hora: "10h", display: "00:09:25", seconds: 565 },
+        { hora: "11h", display: "00:14:22", seconds: 862 },
+        { hora: "12h", display: "00:15:01", seconds: 901 },
+        { hora: "13h", display: "00:14:43", seconds: 883 },
+        { hora: "14h", display: "00:10:22", seconds: 622 },
+        { hora: "15h", display: "00:14:12", seconds: 852 },
+        { hora: "16h", display: "00:15:13", seconds: 913 },
+        { hora: "17h", display: "00:14:44", seconds: 884 },
+        { hora: "18h", display: "00:13:23", seconds: 803 }
+      ],
+      media: { display: "00:12:29", seconds: 749 }
+    },
+    fila: {
+      rows: [
+        { hora: "9h - 10h",  value: 1738.72 },
+        { hora: "10h - 11h", value: 3542.58 },
+        { hora: "11h - 12h", value: 4788.61 },
+        { hora: "12h - 13h", value: 4790.11 },
+        { hora: "13h - 14h", value: 2222.00 },
+        { hora: "14h - 15h", value: 4789.84 },
+        { hora: "15h - 16h", value: 2222.00 },
+        { hora: "16h - 17h", value: 4790.11 },
+        { hora: "17h - 18h", value: 4789.94 }
+      ],
+      media: 2222.00
+    }
+  },
   sim5: {
     label: "Simulação n\u00ba 5",
     direction: "Sentido Leste",
